0c0974ca8a51e6557a1f0f65e66ebfc2eac61130
elijohnsonn <155839906+elijohnsonn@users.noreply.github.com>
2026-09-01 11:49:15 -0600

    fixing netgen report

--- designs/libs/core_tb/layout/lvs_setup.tcl
+++ designs/libs/core_tb/layout/lvs_setup.tcl
@@ -1,5 +1,4 @@
 source /foss/pdks/gf180mcuD/libs.tech/netgen/gf180mcuD_setup.tcl
 ignore class io_secondary_5p0
-property {programmable_folded_cascode} delete
-property {programmable_5tOTA} delete
-property {programmable_common_source} delete
+property nfet_03v3 delete
+property pfet_03v3 delete
